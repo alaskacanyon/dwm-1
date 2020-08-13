349e67ff106fdccbb732053c4ecfcade44e13bb7
alaskacanyon <alaskacanyon@gmail.com>
2020-08-14 01:01:27 +0430

    Update config.h

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"Roboto:size:4" , "monospace:size=4", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char dmenufont[]             = "Roboto:size:6","monospace:size=6";
+static const char *fonts[]          = {"Roboto:size:4", "monospace:size=4", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char dmenufont[]             = {"Roboto:size:6", "monospace:size=6";};
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
